763c0a4458b92f2316751b12c889de328d77852a
thompsonplyler <nealok.g@gmail.com>
2022-12-22 15:12:24 -0500

    none

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// write your code below!
+var name = "Susan";
+var height = 30;
+var message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
